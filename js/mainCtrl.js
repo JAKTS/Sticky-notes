25b0999b571cd9b59598676bee3dc07705131c4c
Michael Davis <endodavis@gmail.com>
2016-07-15 12:33:53 -0600

    Need to add delete function

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,6 +1,21 @@
-angular.module('app').controller('mainCtrl',function($scope,service){
+angular.module('sticky-Notes').controller('mainCtrl',mainCtrl);
 
 
+function mainCtrl($scope,service){
+
+$scope.saveNote = function(note){
+  $scope.note = {};
+    note.service.addNote(note);
+    getNotes();
+};
+
+function getNotes(){
+  service.getNotes().then(function(response){
+    $scope.notes = response;
+  });
+}
+
+getNotes();
 
 
-});
+}
--- js/service.js
+++ js/service.js
@@ -1,6 +1,23 @@
-angular.module('app').service('service', function($http){
+angular.module('sticky-Notes').service('service', service);
+
+function service($http, $q){
 
 
+  this.addNote = function(note){
+  var  existingEntries = JSONParse(localStorage.getItem('notes'));
+    if(!existingEntries) {
+      existingEntries = [];
+    }
+    localStorage.setItem("entry", JSON.stringify(note));
+    existingEntries.push(note);
+    localStorage.setItem('notes',JSON.stringify(note));
+  };
 
+  this.getNotes = function(){
+    var deferred = $q.defer();
+    deferred.resolve(JSONParse(localStorage.getItem('notes')));
 
-});
+    return deferred.promise;
+  };
+
+}
